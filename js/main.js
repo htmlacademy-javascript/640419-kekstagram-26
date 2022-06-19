ea1dc866400b7dc6fb7904a1187d1ca4096fc0a9
Anya Kozyreva <anyakozyreva@gmail.com>
2022-06-19 21:16:50 +1200

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// "use strict" не нужно -- будут модули;
+
+// Вспомогательные функции
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+// https://learn.javascript.ru/task/random-int-min-max
+
+function getRandomIntFromRange(start, end) {
+    let from = start;
+    let to = end;
+
+    if (end < start) {
+        from = end;
+        to = start;
+    }
+
+    if (from >= 0 || start <= end) {
+        return Math.floor(Math.random() * (to - from + 1)) + from;
+    }
+}
+getRandomIntFromRange(100, 140);
+
+// Функция для проверки максимальной длины строки
+// Будет использоваться для проверки длины введённого комментария, но должна быть универсальна
+
+function isLineTooLong(line, length) {
+    return line.length > length;
+}
+isLineTooLong('aaaaa', 140);
