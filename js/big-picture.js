b47b093723838943e55e1a302cda7774bc6b3d27
Anya Kozyreva <anyakozyreva@gmail.com>
2022-07-17 04:50:14 +1200

    Отрисуй меня полностью (часть 2) - почистила комментарии

--- js/big-picture.js
+++ js/big-picture.js
@@ -3,32 +3,14 @@ import { MOCK_NAME_LIST, MOCK_MESSAGE_LIST } from './data.js';
 
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
-
-//  Полноэкранный показ изображения
 const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
-
-//  Кнопка для выхода из полноэкранного просмотра изображения
 const closePictureButton = bigPicture.querySelector('#picture-cancel');
-
-//  Кнопка для загрузки новой порции комментариев
 const commentLoader = bigPicture.querySelector('.comments-loader');
-
-// Информация об изображении. Подпись, комментарии, количество лайков
 const socialCaption = bigPicture.querySelector('.social__caption');
-
-// Комментарии к изображению
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
-
 const socialComments = bigPicture.querySelector('.social__comments');
-
 const socialComment = bigPicture.querySelector('.social__comment');
-
 const commentFragment = document.createDocumentFragment();
-
-/* После открытия окна добавьте
-тегу <body> класс modal-open,
-чтобы контейнер с фотографиями позади не прокручивался при скролле.
-!При закрытии окна не забудьте удалить этот класс. */
 
 const escPicture = () => {
   bigPicture.classList.add('hidden');
@@ -36,13 +18,9 @@ const escPicture = () => {
   document.removeEventListener('keydown', keyPress);
 };
 
-/* Напишите код для закрытия окна
-по нажатию клавиши Esc и клике
-по иконке закрытия */
-
 function keyPress(e) {
   if (e.key === 'Escape') {
-    escPicture();// write your logic here.
+    escPicture();
   }
 }
 
@@ -69,7 +47,6 @@ const renderBigPhoto = ({ url, like, comments, description }) => {
 
   socialComments.innerHTML = '';
   socialComments.appendChild(commentFragment);
-  /*console.log(socialComments); */
   socialCommentCount.classList.add('hidden');
   commentLoader.classList.add('hidden');
 };
